3e8a2f4dedc96aa749d7c12e1304e4e2d4e763f6
Mahmoud Hashem <89657916+0x000029A@users.noreply.github.com>
2022-09-13 20:36:04 +0200

    Update sub.vhd

--- Arithmetic_circuits/Subtraction/subtractor/sub.vhd
+++ Arithmetic_circuits/Subtraction/subtractor/sub.vhd
@@ -11,17 +11,17 @@ end;
 
 architecture synth of SUB_4B is
 	component SUB_1B
-	port(
-		a, b, cin: in std_logic;
-		sub, cout: out std_logic
-	);
+		port(
+			a, b, cin: in std_logic;
+			sub, cout: out std_logic
+		);
 	end component;
 	signal cout_internal: std_logic_vector(3 downto 0);
 begin
-	FS_4B_0: SUB_1B port map(a(0), not b(0), '1',              sub(0), cout_internal(0));
-	FS_4B_1: SUB_1B port map(a(1), not b(1), cout_internal(0), sub(1), cout_internal(1));
-	FS_4B_2: SUB_1B port map(a(2), not b(2), cout_internal(1), sub(2), cout_internal(2));
-	FS_4B_3: SUB_1B port map(a(3), not b(3), cout_internal(2), sub(3), open);
+	SUB_4B_0: SUB_1B port map(a(0), not b(0), '1',              sub(0), cout_internal(0));
+	SUB_4B_1: SUB_1B port map(a(1), not b(1), cout_internal(0), sub(1), cout_internal(1));
+	SUB_4B_2: SUB_1B port map(a(2), not b(2), cout_internal(1), sub(2), cout_internal(2));
+	SUB_4B_3: SUB_1B port map(a(3), not b(3), cout_internal(2), sub(3), open);
 end;
 
 ---------1-bit Subtractor----------
