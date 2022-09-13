f021ac63beba2dda1cc71cb65e79ef72f3110a5a
Mahmoud Hashem <89657916+0x000029A@users.noreply.github.com>
2022-09-13 19:22:40 +0200

    Update sub.vhd

--- Arithmetic_circuits/Subtraction/subtractor/sub.vhd
+++ Arithmetic_circuits/Subtraction/subtractor/sub.vhd
@@ -2,15 +2,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
-entity FS_4B is
+entity SUB_4B is
 	port(
 		a, b: in std_logic_vector(3 downto 0);
 		sub: out std_logic_vector(3 downto 0)
 	);
 end;
 
-architecture synth of FS_4B is
-	component FS_1B
+architecture synth of SUB_4B is
+	component SUB_1B
 	port(
 		a, b, cin: in std_logic;
 		sub, cout: out std_logic
@@ -18,24 +18,24 @@ architecture synth of FS_4B is
 	end component;
 	signal cout_internal: std_logic_vector(3 downto 0);
 begin
-	FS_4B_0: FS_1B port map(a(0), not b(0), '1',              sub(0), cout_internal(0));
-	FS_4B_1: FS_1B port map(a(1), not b(1), cout_internal(0), sub(1), cout_internal(1));
-	FS_4B_2: FS_1B port map(a(2), not b(2), cout_internal(1), sub(2), cout_internal(2));
-	FS_4B_3: FS_1B port map(a(3), not b(3), cout_internal(2), sub(3), open);
+	FS_4B_0: SUB_1B port map(a(0), not b(0), '1',              sub(0), cout_internal(0));
+	FS_4B_1: SUB_1B port map(a(1), not b(1), cout_internal(0), sub(1), cout_internal(1));
+	FS_4B_2: SUB_1B port map(a(2), not b(2), cout_internal(1), sub(2), cout_internal(2));
+	FS_4B_3: SUB_1B port map(a(3), not b(3), cout_internal(2), sub(3), open);
 end;
 
 ---------1-bit FULL SUBTRACTOR----------
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
-entity FS_1B is
+entity SUB_1B is
 	port(
 		a, b, cin: in std_logic;
 		sub, cout: out std_logic
 	);
 end;
 
-architecture synth of FS_1B is
+architecture synth of SUB_1B is
 begin
 	cout <= (a and b) or (a and cin) Or (cin and b);
 	sub <= a xor b xor cin;
