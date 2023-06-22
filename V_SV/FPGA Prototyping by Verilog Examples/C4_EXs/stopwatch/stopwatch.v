049b2458507dd1c68dd18fb59a3fe2078fef8fc4
Mahmoud Hashem <89657916+0x000029A@users.noreply.github.com>
2023-06-22 19:06:08 +0300

    Fixed a bug

--- V_SV/FPGA Prototyping by Verilog Examples/C4_EXs/stopwatch/stopwatch.v
+++ V_SV/FPGA Prototyping by Verilog Examples/C4_EXs/stopwatch/stopwatch.v
@@ -4,9 +4,6 @@ module stopwatch
   output wire [3:0] d3,  d2,  d1,  d0
 );
 
-reg [3:0] d0_current, d1_current, d2_current, d3_current;
-reg [3:0] d0_next, d1_next, d2_next, d3_next;
-
 // counter
 reg counter_rst;
 wire max_tick;
@@ -19,72 +16,66 @@ parameter S0 = 2'b00,
           S3 = 2'b11;
 
 reg [1:0] current_state, next_state;
+reg [9:0] _01s_counter_current, _1s_counter_current, _10s_counter_current, _60s_counter_current;
+reg [9:0] _01s_counter_next,    _1s_counter_next,    _10s_counter_next,    _60s_counter_next;
 
 always @(posedge clk)
 begin
   if (rst)
   begin
-    current_state <= S0;
-    d0_current <= 0;
-    d1_current <= 0;
-    d2_current <= 0;
-    d3_current <= 0;
+    current_state        <= S0;
+    _01s_counter_current <= 0;
+    _1s_counter_current  <= 0;
+    _10s_counter_current <= 0;
+    _60s_counter_current <= 0;
   end
   else
   begin
-    current_state <= next_state;
-    d0_current <= d0_next;
-    d1_current <= d1_next;
-    d2_current <= d2_next;
-    d3_current <= d3_next;
+    current_state        <= next_state;
+    _01s_counter_current <= _01s_counter_next;
+    _1s_counter_current  <= _1s_counter_next;
+    _10s_counter_current <= _10s_counter_next;
+    _60s_counter_current <= _60s_counter_next;
   end
 end
 
 // next state logic
 always @*
 begin
+  _01s_counter_next = _01s_counter_current;
+  _1s_counter_next  = _1s_counter_current;
+  _10s_counter_next = _10s_counter_current;
+  _60s_counter_next = _60s_counter_current;
   case(current_state)
     S0:
     begin
       counter_rst = 1'b1;
-      d0_next = d0_current;
-      d1_next = d1_current;
-      d2_next = d2_current;
-      d3_next = d3_current;
       next_state = go && up ? S1 : go && !up ? S2 : S0;
     end
     S1:
     begin
       counter_rst = 1'b0;
 
-      if (d0_current == 9)
-        d0_next = d0_current;
-      else if (max_tick)
-        d0_next = d0_current + 1;
+      if (max_tick && !(_01s_counter_current == 9))
+        _01s_counter_next = _01s_counter_current + 1;
 
-      if (d0_current == 9)
-        if (d1_current == 9)
-          d1_next = d1_current;
-        else if (max_tick)
-          d1_next = d1_current + 1;
-      else
-        d1_next = d1_current;
+      if ((_01s_counter_current == 9) && max_tick && !(_1s_counter_current == 10))
+      begin
+        _01s_counter_next = 0;
+        _1s_counter_next = _1s_counter_current + 1;
+      end
 
-      if (d1_current == 9)
-        if (d2_current == 5)
-          d2_next = d2_current;
-        else if (max_tick)
-          d2_next = d2_current + 1;
-      else
-        d2_next = d2_current;
+      if ((_1s_counter_current == 10) && max_tick && !(_10s_counter_current == 6))
+      begin
+        _1s_counter_next = 0;
+        _10s_counter_next = _10s_counter_current + 1;
+      end
 
-      if (d2_current == 5)
-        if (d3_current == 9)
-          d3_next = d3_current;
-        else if (max_tick)
-          d3_next = d3_current + 1;
-      else
-        d3_next = d3_current;
+      if ((_10s_counter_current == 6) && max_tick && !(_60s_counter_current == 10))
+      begin
+        _10s_counter_next = 0;
+        _60s_counter_next = _60s_counter_current + 1;
+      end
 
       next_state = max_tick ? S3 : go && up ? S1 : go && !up ? S2 : S0;
     end
@@ -92,46 +83,26 @@ begin
     begin
       counter_rst = 1'b0;
 
-      if (d3_current == 0)
-        d3_next = d3_current;
-      else if (max_tick)
-        if (d3_current == 0)
-          d3_next = d3_current;
-        else
-          d3_next = d3_current - 1;
+      if (max_tick && !(_01s_counter_current == 0))
+        _01s_counter_next = _01s_counter_current - 1;
 
-      if (d3_current == 0)
-        if (max_tick)
-          if (d2_current == 0)
-            d2_next = d2_current;
-          else
-            d2_next = d2_current - 1;
-        else
-          d2_next = d2_current;
-      else
-        d2_next = d2_current;
-      
-      if (d2_current == 0)
-        if (max_tick)
-          if (d1_current == 0)
-            d1_next = d1_current;
-          else
-            d1_next = d1_current - 1;
-        else
-          d1_next = d1_current;
-      else
-        d1_next = d1_current;
+      if ((_01s_counter_current == 0) && max_tick && !(_1s_counter_current == 0))
+      begin
+        _01s_counter_next = 9;
+        _1s_counter_next = _1s_counter_current - 1;
+      end
 
-      if (d1_current == 0)
-        if (max_tick)
-          if (d0_current == 0)
-            d0_next = d0_current;
-          else
-            d0_next = d0_current - 1;
-        else
-          d0_next = d0_current;
-      else
-        d0_next = d0_current;
+      if ((_1s_counter_current == 0) && max_tick && !(_10s_counter_current == 0))
+      begin
+        _1s_counter_next = 9;
+        _10s_counter_next = _10s_counter_current - 1;
+      end
+
+      if ((_10s_counter_current == 0) && max_tick && !(_60s_counter_current == 0))
+      begin
+        _10s_counter_next = 5;
+        _60s_counter_next = _60s_counter_current - 1;
+      end
 
       next_state = max_tick ? S3 : go && up ? S1 : go && !up ? S2 : S0;
     end
@@ -146,10 +117,10 @@ end
 
 
 // output logic
-assign d0 = d0_current;
-assign d1 = d1_current;
-assign d2 = d2_current;
-assign d3 = d3_current;
+assign d0 = _01s_counter_current;
+assign d1 = _1s_counter_current;
+assign d2 = _10s_counter_current;
+assign d3 = _60s_counter_current;
 
 endmodule
 
@@ -169,6 +140,6 @@ always @(posedge clk)
     current_state <= next_state;
 
 assign next_state = current_state + 1;
-assign max_tick = (current_state == 3) ? 1'b1 : 1'b0;
+assign max_tick   = (current_state == 3) ? 1'b1 : 1'b0;
 
 endmodule
